c5dbf6ab945c62543d1c026c0de27b59cb9cb197
Noah Ratcliff <me@nratcliff.com>
2018-06-17 10:51:37 -0400

    create EventType struct and Events SO

---
+++ Scripts/Data/EventType.cs
@@ -0,0 +1,26 @@
+﻿namespace Plugboard.Data
+{
+    public struct EventType
+    {
+        /// <summary>
+        /// The human-readable name of this event.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// The unique ID of this event.
+        /// </summary>
+        public int ID { get; private set; }
+        
+        /// <summary>
+        /// Defines a new event type
+        /// </summary>
+        /// <param name="name">The human-readable name of the event</param>
+        /// <param name="id">The unique ID of this event</param>
+        internal EventType(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+    }
+}
---
+++ Scripts/Data/Events.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace Plugboard
+{
+    /// <summary>
+    /// Holds list of <see cref="Data.EventType"/>s.
+    /// </summary>
+    public sealed class Events : ScriptableObject
+    {
+        /// <summary>
+        /// All defined event types, set via custom property drawer.
+        /// </summary>
+        [SerializeField, HideInInspector]
+        private Data.EventType[] eventTypes;
+
+        /// <summary>
+        /// Finds an <see cref="Data.EventType"/> by name. Not very efficient.
+        /// </summary>
+        /// <param name="name">The name of the event</param>
+        public Data.EventType FindByName(string name)
+        {
+            for (int i = 0; i < eventTypes.Length; i++)
+            {
+                if (eventTypes[i].Name.Equals(name))
+                    return eventTypes[i];
+            }
+
+            // TODO: NoSuchEvent exception or something like that
+            throw new Exception("No event with name: " + name);
+        }
+
+        /// <summary>
+        /// Gets an <see cref="Data.EventType"/> by ID.
+        /// </summary>
+        public Data.EventType this [int i]
+        {
+            get { return eventTypes[i]; }
+        }
+
+        /// <summary>
+        /// Finds an EventType by name. Not very efficient.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Data.EventType this [string name]
+        {
+            get { return FindByName(name); }
+        }
+    }
+}
